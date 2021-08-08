45109ff0e7656c87a7c1f1d742ab911a855b8bee
Amir60234 <mohamedamir60234@gnail.com>
2021-08-08 23:39:14 +0600

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,169 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .m4a formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "আমারো পরাণো যাহা চায়",
+    artist: "রবীন্দ্র সংগীত",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "মধ্যরাতে ডাকলে দিও সাড়া",
+    artist: "তালপাতার সেপাই",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Asmano pe jo khuda hai",
+    artist: "Jubin Nautiyal",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Waka Waka",
+    artist: "Shakira",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "এ হৃদয় ভেঙে গেলে  জানো কি তা  লাগে না লাগে না জোড়া",
+    artist: "Khalid",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "আমি শুধু খুঁজেছি আমায়",
+    artist: "তালপাতার সেপাই",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Give me some sunshine",
+    artist: "3 idiots",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tuj mein rab dikhta hai",
+    artist: "Rab ne bana di jodi",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "তুমি চাইলে বৃষ্টি মেঘও ছিলো রাজি",
+    artist: "Minar Rahman",
+    img: "music-4",
+    src: "music-4"
+  },
+  
+  
+  {
+    name: "যদি তোর ডাক শুনে কেউ না আসে",
+    artist: "রাবীন্দ্র সংগীত",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Structure mood off song",
+    artist: "Unknown",
+    img: "music-8",
+    src: "music-8"
+  },
+  
+  
+  {
+    name: "আমি চেয়ে চেয়ে দেখি সারাদিন",
+    artist: "Miftah Zaman",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "আলো আলো",
+    artist: "Tahsan Khan",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "কাটে না সময় যখন আর কিছুতে",
+    artist: "Hemanta Mukherjee",
+    img: "music-13",
+    src: "music-13"
+  },
+ 
+  {
+    name: "বেলা বোস",
+    artist: "Anjun Dutta",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "বৃদ্ধাশ্রম",
+    artist: "Nachiketa Chakraborty",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "সে প্রথম প্রেম আমার নীলাঞ্জনা",
+    artist: "Nachiketa Chakraborty",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "অতীতটাকে বাক্সে পুরে",
+    artist: "Nachiketa Chakraborty",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "কারার ঐ লৌহকপাট",
+    artist: "নজরুল গীতি",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "হতেও পারে এই দেখা শেষ দেখা",
+    artist: "Jams",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "সপ্ন যাবে বাড়ি",
+    artist: "Grameephone",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "আমাকে আমার মতো থাকতে দাও",
+    artist: "Anupam Roy",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "লক্ষীটি",
+    artist: "Anupam Roy",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "তুমি যাকে ভালবাসো",
+    artist: "Iman Chakraborty",
+    img: "music-24",
+    src: "music-24"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
